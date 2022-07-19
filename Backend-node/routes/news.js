1d145198f28d05f29bf4381f08d2d2203c235c31
naim6246 <uddinnaim895@gmail.com>
2022-07-19 18:07:51 +0600

    .

--- Backend-node/routes/news.js
+++ Backend-node/routes/news.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const newsHandler = require('../handlers/news');
+const auth = require('../auth/authentication');
 
 router = express.Router();
 
 router.route('/')
-    .get(newsHandler.getAllNews)
-    .post(newsHandler.createNews);
+    .get(auth.Autheticate,newsHandler.getAllNews)
+    .post(auth.Autheticate,newsHandler.createNews);
 
 router.route('/:newsID')
-    .get(newsHandler.getNewsByID)
-    .put(newsHandler.updateNews)
-    .delete(newsHandler.deleteNews);
+    .get(auth.Autheticate,newsHandler.getNewsByID)
+    .put(auth.Autheticate,newsHandler.updateNews)
+    .delete(auth.Autheticate,newsHandler.deleteNews);
 
 module.exports = router;
